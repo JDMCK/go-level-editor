0664e3a39baff34992dd5209f11a7048ec96ff50
Jesse McKenzie <jesse@jessemckenzie.com>
2026-06-13 19:58:20 -0700

    Use zenity file picker for map import
    Add zenity-based file selection when importing maps: import github.com/ncruces/zenity, introduce IsImport flag and, when set, prompt the user with a file dialog and set MapImportPath before calling ImportMap().

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	eb "github.com/hajimehoshi/ebiten/v2"
+	"github.com/ncruces/zenity"
 )
 
 var AtlasPath *string
@@ -13,15 +14,26 @@ var TileSize *int
 var TileWidth int
 var TileHeight int
 var ImportedLayerIndices [][]int
+var IsImport *bool
 
 func main() {
 	AtlasPath = flag.String("path", "", "File path to atlas relative to the folder this program is being run from.")
-	MapImportPath = flag.String("map", "", "File path to map file (for importing and editing a saved level).")
+	IsImport = flag.Bool("import", true, "File path to map file (for importing and editing a saved level).")
 	TileSize = flag.Int("tile_size", 0, "The width and height in pixels of the tiles in the atlas (only square tiles supported).")
 
 	flag.Parse()
 
-	if *MapImportPath != "" {
+	if *IsImport == true {
+		filePath, _ := zenity.SelectFile(
+			zenity.Title("Load Level"),
+			zenity.FileFilters{
+				{
+					Name:     "Map Files",
+					Patterns: []string{"*.map"},
+				},
+			},
+		)
+		MapImportPath = &filePath
 		ImportMap()
 	} else if *AtlasPath == "" ||
 		*TileSize == 0 {
